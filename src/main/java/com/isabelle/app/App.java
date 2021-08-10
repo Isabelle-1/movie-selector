0c9e9c440e69f8fd3fd8e2d4b140799c6dd1a84e
Isabelle-1 <isabelle.lathlean@gmail.com>
2021-08-11 01:40:09 +1000

    added if else statement for adventurous and other input options

--- src/main/java/com/isabelle/app/App.java
+++ src/main/java/com/isabelle/app/App.java
@@ -15,22 +15,6 @@ public class App {
         Movie sample = new Movie("happy", "Sleeping beauty");
         sample.Play();
       
-//    //ask user to input mood
-//        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-//        System.out.println("Please select a mood from the following list");
-//        System.out.println("eg. happy 3");
-//        System.out.println("Need a laugh: happy");
-//        System.out.println("Need a good cry: sad");
-//        System.out.println("Want some adventure: adventurous");
-//        System.out.println("Other");
-//
-//        String moods = myObj.nextLine();  // Read user input
-//        System.out.println("how many suggestions would you like"); 
-//        int numberOfMovies = myObj.nextInt();
-//        System.out.println("Mood is: " + mood);  // Output user input
-        
-        
-   	//array list to store possible movies
 
     	
     	
